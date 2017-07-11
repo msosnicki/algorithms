3439a6897defeede3575b53637e47a62085d5ace
msosnicki <msosnicki@ucs.imshealth.com>
2017-07-11 16:24:06 +0200

    Last number of a sum of large fibonacci

--- app/week2/Lastdigitfib.hs
+++ app/week2/Lastdigitfib.hs
@@ -8,15 +8,30 @@ main :: IO ()
 main =
   hSetBuffering stdin NoBuffering >>= \_ ->
   nextNum >>= \num ->
-  putStrLn $ show $ fibs !! num
+  putStrLn $ show $ lastDigitOfSum num
 
-fibs :: [Integer]
-fibs  = map (`mod` 10) $ scanl (+) 0 (1:fibs)
+lastDigitOfSum n =
+  let pp = pisanoPeriod 10
+      s = n `div` pp
+      r = n `rem` pp
+      sum1 = (s*) $ sum $ take pp fibsMod10
+      sum2 = sum $ take r fibsMod10
+  in sum1 + sum2 `mod` 10
 
-nextNum :: (Integral a, Read a) => IO a
+pisanoPeriod m =
+  let pp l acc = case l of
+        (0:1:_)    -> acc + 2
+        (_:0:t)    -> let acc' = 1 + acc in seq acc' $ pp (0:t) acc'
+        (h1:h2:t)  -> let acc' = 2 + acc in seq acc' $ pp t acc'
+  in pp (drop 2 $ fibsMod m) 0
+
+fibsMod10 = fibsMod 10
+fibsMod m = map (`mod` m) fibs
+
+fibs  = scanl (+) 0 (1:fibs)
+
 nextNum = nextNum' ""
 
-nextNum' :: (Integral a, Read a) => String -> IO a
 nextNum' n = getChar >>= \char ->
   if(isDigit char) then nextNum' $ char:n
   else if(null n) then nextNum' n

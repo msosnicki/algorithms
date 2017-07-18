c0884116f35366912473a75e408c6af72a1bf1a1
msosnicki <msosnicki@ucs.imshealth.com>
2017-07-18 15:58:57 +0200

    5/6.. still not passing

--- app/week3/Problem6.hs
+++ app/week3/Problem6.hs
@@ -4,7 +4,6 @@ import Data.Char
 import Data.List
 import Data.Maybe
 import Control.Applicative
-import Control.Monad
 import System.IO
 
 -- Given two sequences a1, a2.. an (ai is the profit per click of the i-th ad) and b1, b2, . . . , bn (bi is
@@ -18,13 +17,30 @@ main =
   nextNums n >>= \nums ->
   putStrLn $ show $ solve nums 
 
-solve :: [Integer] -> Integer
-solve nums =
-  let strings = join $ map show nums
-      digits = map (\s -> read $ s : []) strings
-      getDigit num a = num * 10 + a
-  in foldl' getDigit 0 $ sortBy (flip compare) digits
+solve nums = toNum $ reverse $ sortBy order nums
 
+toNum :: (Integral a, Show a) => [a] -> a
+toNum = foldl' f 0
+  where f acc elem =
+          let n = (10^) $ (1+) $ pow elem
+          in acc * n + elem
+
+order :: Integer -> Integer -> Ordering
+order a b
+  | a == b = EQ
+  | ah == bh = (fromMaybe ah at) `order` (fromMaybe bh bt)
+  | otherwise = compare ah bh
+  where (ah, at) = mostImportant a
+        (bh, bt) = mostImportant b
+
+mostImportant :: Integer -> (Integer, Maybe Integer)
+mostImportant num =
+  let n = pow num
+      (m, r) = divMod num $ 10^n
+  in (m, if(r == 0) then Nothing else Just r)
+
+pow num = (-1+) $ length $ show num
+  
 nextNums :: (Integral a, Read a) => Int -> IO [a]
 nextNums n = sequence $ take n $ repeat nextNum
 

afc2223abf04549315afdb67d536bb6fab0a0853
msosnicki <essn87@gmail.com>
2017-08-10 15:29:13 +0200

    not passing time

--- app/week5/Problem1.hs
+++ app/week5/Problem1.hs
@@ -3,6 +3,7 @@ module Problem1 where
 import Data.Char
 import Data.List
 import Data.Array
+import Data.Maybe
 import Control.Applicative
 import System.IO
 
@@ -12,20 +13,43 @@ main :: IO ()
 main =
   hSetBuffering stdin NoBuffering >>= \_ ->
   nextNum >>= \n ->
-  putStrLn $ show $ solve n
+  let result = solve n
+      printSize = putStrLn $ show $ length result - 1
+      printList = putStrLn $ intercalate " " (show <$> result)
+  in printSize >>= \_ -> printList
+  
 
-solve :: Integer -> Integer
-solve n = solution ! n
+solve :: Integer -> [Integer]
+solve n = reverse $ solution ! n
   where
-    solution = array (1, n) $ (1, 0) : [
-      (i, best + 1) | i <- [2..n],
-        let best = minimum $ (solution !) <$> (ind i)
+    solution = array (1, n) $ (1, [1]) : [
+      (i, (op $ fromJust index) : (fromJust acc)) | i <- [2..n],
+        let solutions = sol i solution <$> ops
+            sorted = sortBy sorting solutions
+            (op, index, acc) = head sorted
       ]
 
-ind n =
-  let is = [(n `divMod` 3), (n `divMod` 2), (n - 1, 0)]
-      f = filter ((==0) . snd) $ filter ((>0) .fst) is
-  in fst <$> f
+sorting (_, _, Nothing) (_, _, l2) = GT
+sorting (_, _, l1) (_, _, Nothing) = LT
+sorting (_, _, Just l1) (_, _, Just l2) = compare (length l1) (length l2)
+
+data Op = Op {function :: Integer -> Integer, inverse :: Integer -> Maybe Integer}
+
+ops = [
+  Op (+1) (Just . (subtract 1)),
+  Op (*3) (invMul 3),
+  Op (*2) (invMul 2)
+      ]
+
+sol num arr op =
+  let i = inverse op num
+  in (function op, i, (arr !) <$> i)
+
+invMul times = f
+  where f num =
+          let (d, r) = num `divMod` times
+          in if(r == 0) then Just d else Nothing
+  
 
 nextNums :: (Integral a, Read a) => Int -> IO [a]
 nextNums n = sequence $ replicate n nextNum
